0df1672cde3b5e91e06b11c1c5611facc51790c9
RashedCSEJnU <b190305022@cse.jnu.ac.bd>
2024-11-27 12:29:19 +0600

    add two good problem QED_Favorite Permutation

---
+++ CodeForces/D_QED_s_Favorite_Permutation.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n, q;
+        cin >> n >> q;
+
+        vector<int> v(n + 1), preMax(n + 1);
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> v[i];
+            preMax[i] = max(v[i], preMax[i - 1]);
+        }
+
+        string s;
+        cin >> s;
+        s = '#' + s;
+
+        int badBlock = 0;
+        auto func = [&](int i, int x)
+        {
+            if (i < n && s[i] == 'L' && s[i + 1] == 'R' && preMax[i] > i)
+                badBlock += x;
+        };
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (preMax[i] > i)
+                func(i, 1);
+        }
+
+        while (q--)
+        {
+            int i;
+            cin >> i;
+
+            func(i, -1), func(i - 1, -1);
+
+            s[i] = (s[i] == 'R' ? 'L' : 'R');
+
+            func(i, 1), func(i - 1, 1);
+
+            cout << (!badBlock ? "YES\n" : "NO\n");
+        }
+    }
+    return 0;
+}
--- code.cpp
+++ code.cpp
@@ -1,15 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+void solve()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> p(n);
+    for (auto &pi : p)
+    {
+        cin >> pi;
+    }
+    string s;
+    cin >> s;
+    vector<int> pref(n + 1);
+    for (int i = 0; i < n; ++i)
+    {
+        pref[i + 1] = max(pref[i], p[i]);
+    }
+    int bad = 0;
+
+    auto yo = [&](int i, int x)
+    {
+        if (i == n - 1)
+            return;
+        if (s[i] == 'L' and s[i + 1] == 'R')
+        {
+            if (pref[i + 1] > i + 1)
+            {
+                bad += x;
+            }
+        }
+    };
+
+    for (int i = 0; i + 1 < n; ++i)
+    {
+        yo(i, 1);
+    }
+
+    while (q--)
+    {
+        int i;
+        cin >> i;
+        i--;
+        yo(i - 1, -1);
+        yo(i, -1);
+        if (s[i] == 'L')
+            s[i] = 'R';
+        else
+            s[i] = 'L';
+        yo(i - 1, 1);
+        yo(i, 1);
+        if (!bad)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(0), cin.tie(0);
-
-    int t;
+    ios::sync_with_stdio(0), cin.tie(0);
+    int t = 1;
     cin >> t;
-    for (int tc = 1; tc <= t; tc++)
+    for (int i = 1; i <= t; ++i)
     {
-        int p, s, t;
-        cin >> p >> s >> t;
+        solve();
     }
+    return 0;
 }
