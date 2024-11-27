2ea9563d99d69e62673de0f8a51a798d01ebaed0
RashedCSEJnU <b190305022@cse.jnu.ac.bd>
2024-11-28 02:32:38 +0600

    add QED again

--- CodeForces/D_QED_s_Favorite_Permutation.cpp
+++ CodeForces/D_QED_s_Favorite_Permutation.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-<<<<<<< HEAD
 int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0);
@@ -40,97 +39,12 @@ int main()
             int i;
             cin >> i;
 
-            func(i, -1), func(i - 1, -1);
-
+            func(i, -1), func(i - 1, -1); // remove
             s[i] = (s[i] == 'R' ? 'L' : 'R');
-
-            func(i, 1), func(i - 1, 1);
+            func(i, 1), func(i - 1, 1); // add
 
             cout << (!badBlock ? "YES\n" : "NO\n");
         }
     }
-=======
-class Solution
-{
-private:
-#include <bits/stdc++.h>
-
-    void tabulation(string &s1, string &s2, vector<vector<int>> &dp)
-    {
-        int n1 = s1.size(), n2 = s2.size();
-        for (int i = 1; i <= n1; i++)
-        {
-            for (int j = 1; j <= n2; j++)
-            {
-                if (s1[i - 1] == s2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-    string getLCS(string &s, string &t, vector<vector<int>> &dp)
-    {
-        string ans;
-        int i = s.length(), j = t.length();
-        while (i && j)
-        {
-            if (s[i - 1] == t[j - 1])
-            {
-                ans.push_back(s[i - 1]);
-                i--, j--;
-            }
-            else if (dp[i - 1][j] > dp[i][j - 1])
-                i--;
-            else
-                j--;
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
-
-public:
-    // Function to find length of shortest common supersequence of two strings.
-    int shortestCommonSupersequence(string &s, string &t)
-    {
-        int n1 = s.size(), n2 = t.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
-        tabulation(s, t, dp);
-        string common = getLCS(s, t, dp);
-        if (common.length() == 0)
-            return s.length() + t.length();
-        string ans;
-        int n3 = common.length(), i = 0, j = 0, k = 0;
-        while (i < n1 && j < n2 && k < n3)
-        {
-            if (s[i] == t[j])
-            {
-                ans.push_back(s[i]);
-                i++, j++, k++;
-            }
-            else if (s[i] == common[k])
-                ans.push_back(t[j++]);
-            else if (t[j] == common[k])
-                ans.push_back(s[i++]);
-            else
-            {
-                ans.push_back(s[i++]);
-                ans.push_back(t[j++]);
-            }
-        }
-        while (i < n1)
-            ans.push_back(s[i++]);
-        while (j < n2)
-            ans.push_back(t[j++]);
-        return ans.length();
-    }
-};
-int main()
-{
-    string s, t;
-    cin >> s >> t;
-    Solution ob;
-    cout << ob.shortestCommonSupersequence(s, t) << '\n';
->>>>>>> 01d9dd3 (QED)
     return 0;
 }
