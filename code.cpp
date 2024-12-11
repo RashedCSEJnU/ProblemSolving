f1da32e4c3fdd585519584d63b8f311aee73ffeb
RashedCSEJnU <b190305022@cse.jnu.ac.bd>
2024-12-11 11:50:16 +0600

    new commit

--- code.cpp
+++ code.cpp
@@ -1,52 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
+
+int gcd(int a, int b)
+{
+    return __gcd(a, b);
+}
+int lcm(int a, int b)
+{
+    return (a * b) / gcd(a, b);
+}
 
 int32_t main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0);
-    int t = 1;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int> a(n), b(n);
-        vector<pair<int, int>> v(n);
-        for (int &i : a)
-            cin >> i;
-        for (int &i : b)
-            cin >> i;
-
-        multiset<pair<int, int>> ms;
-        for (int i = 0; i < n; i++)
-            ms.insert({a[i], b[i]});
-
-        int sum = LONG_LONG_MIN;
-        pair<int, int> p;
-        for (auto it : ms)
-        {
-            int tempSum = it.first + it.second;
-            if (tempSum > sum)
-            {
-                sum = tempSum;
-                p = it;
-            }
-        }
-        ms.erase(p);
-
-        int ans1 = 0, ans2 = 0, ans3 = 0, MiddleSum = p.first + p.second;
-        for (auto it : ms)
-        {
-            ans2 += it.first;
-            ans3 += it.second;
-            if (it.first > it.second)
-                ans1 += it.first;
-            else
-                ans1 += it.second;
-        }
-        int ans = max({ans1 + MiddleSum, ans2 + MiddleSum, ans3 + MiddleSum});
-        cout << ans << '\n';
-    }
-    return 0;
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << ' ' << lcm(a, b) << endl;
 }
